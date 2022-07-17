a16b9335a9947f704e527f49346b0fd406809f17
Lucas Furquim <lucasviniciusfurquim@gmail.com>
2022-07-17 00:57:59 -0300

    finalizando animais-fantasticos

--- projetos/Animais-Fantasticos/js/script.js
+++ projetos/Animais-Fantasticos/js/script.js
@@ -3,9 +3,15 @@ import Accordion from "./modulos/accordion.js";
 import anima from "./modulos/anima.js";
 import desc from "./modulos/anima-desc.js";
 import endereco from "./modulos/endereco.js";
+import horario from "./modulos/horario.js";
+import dropMenu from "./modulos/dropMenu.js";
+import numeros from "./modulos/numeros.js";
 
 imagens();
 Accordion();
 anima();
 desc();
 endereco();
+horario();
+dropMenu();
+numeros();
